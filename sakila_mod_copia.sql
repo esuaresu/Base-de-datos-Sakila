ff7808547cd32d100b8d46e23e4b42c6d45ba367
esuaresu <esuaresu@gmail.com>
2026-04-10 10:42:19 +0200

    Add data
    Created tables for actor and film with initial data.

--- sakila_mod_copia.sql
+++ sakila_mod_copia.sql
@@ -1,1 +1,19 @@
-OCI runtime exec failed: exec failed: unable to start container process: exec: "mysqldump": executable file not found in $PATH: unknown
+CREATE TABLE actor (
+  actor_id int NOT NULL AUTO_INCREMENT,
+  first_name varchar(45),
+  last_name varchar(45),
+  birth_year int,
+  PRIMARY KEY (actor_id)
+);
+
+INSERT INTO actor VALUES
+(1,'PENELOPE','GUINESS',1970),
+(2,'NICK','WAHLBERG',1965);
+
+CREATE TABLE film (
+  film_id int NOT NULL AUTO_INCREMENT,
+  title varchar(100),
+  release_year int,
+  rating varchar(10),
+  PRIMARY KEY (film_id)
+);
